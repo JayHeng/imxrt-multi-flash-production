d181f0044f21346c3504c8cded6fdc1b0added2d
Jay Heng <hengjie1989@foxmail.com>
2024-05-28 16:35:51 +0800

    Add default dummy use option for QuadSPI
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- boards/mcu/mfb_fw/src/mfb_nor_flash_winbond.h
+++ boards/mcu/mfb_fw/src/mfb_nor_flash_winbond.h
@@ -59,6 +59,7 @@
 // In SPI mode, SPI Set Read Parameters (C0h)" instruction writes to ¡®Dummy Clocks¡¯ P[6:4] bits only, while it 
 //    will ignore ¡®Wrap Length¡¯ P[1:0] bits input as they are don¡¯t care in SPI mode.
 #define WINBOND_QUAD_FLASH_SET_DUMMY_CMD 0x30
+#if MFB_FLASH_USE_DEFAULT_DUMMY
 #define WINBOND_QUAD_FLASH_DUMMY_CYCLES  0x06
 #else
 #define WINBOND_QUAD_FLASH_DUMMY_CYCLES  0x08
